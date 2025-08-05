43a511b6a555f54a4e9ffee75c48e66345af743d
AM-Tech0 <amanbsdm6794@gmail.com>
2025-08-05 11:46:15 +0530

    hlo

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Footer from "./container/footer/Footer.jsx";
 import Header from "./container/header/Header.jsx";
 import Experience from "./container/Experience/Experience.jsx";
 import Testimonials from "./container/testimonials/Testimonials.jsx";
-import Navbar from './Component/Navbar/Navbar.jsx'
-import Portfolio from "./container/Portfolio/Portfolio.jsx";
+import Navbar from './component/Navbar/Navbar.jsx'
+import Portfolio from "./container/portfolio/Portfolio.jsx";
 import Contact from "./container/Contact/Contact.jsx";
 
 function App() {
