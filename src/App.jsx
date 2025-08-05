eee2967eaae28179f662f34a781fdbb62fb87d6c
AM-Tech0 <amanbsdm6794@gmail.com>
2025-08-05 10:41:36 +0530

    hlo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import React from "react";
 import About from "./container/About/About.jsx";
-import Footer from "./container/Footer/Footer.jsx";
-import Header from "./container/Header/Header.jsx";
+import Footer from "./container/footer/Footer.jsx";
+import Header from "./container/header/Header.jsx";
 import Experience from "./container/Experience/Experience.jsx";
 import Testimonials from "./container/testimonials/Testimonials.jsx";
 import Navbar from './Component/Navbar/Navbar.jsx'
